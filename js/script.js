7fe82caef30654f19da81dcd3f3eae40df7a4d68
Alpha DIOP <alphakhoss@gmail.com>
2024-08-21 10:59:50 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  if (name && email && message) {
+    document.getElementById("responseMessage").textContent = "Merci, " + name + "! Votre message a été envoyé.";
+    document.getElementById("responseMessage").style.color = "green";
+    // Réinitialiser le formulaire
+    document.getElementById("contactForm").reset();
+  } else {
+    document.getElementById("responseMessage").textContent = "Veuillez remplir tous les champs.";
+    document.getElementById("responseMessage").style.color = "red";
+  }
+});
